1478eb6fa4ec918d75dd531921fd55e244e51e5b
Varya Sachenko <ya.varushka@ya.ru>
2021-08-15 16:52:08 +0300

    first 11

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { classApi as api } from '../utils/aaapi'
+import { classApi as api } from '../utils/api'
 import Card from './Card';
 
 import { CurrentUserContext} from '../contexts/CurrentUserContext';
