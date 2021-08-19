3ee0ac605ec99dcc4d6ad3abbd5ee7f877474bbb
Varya Sachenko <ya.varushka@ya.ru>
2021-08-19 20:05:54 +0300

    4 11

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -37,11 +37,11 @@ export default function EditProfilePopup(props) {
         <PopupWithForm buttonText="Сохранить" onSubmit={handleSubmit} onClose={props.onClose} isOpen={props.isOpen} title="Редактировать профиль" name="field_edit" children={
             <>
                 <input name="name" className="form__field-text form__field-text_input_name" placeholder="Имя" type="text"
-                    size="40" id="username" required minLength="2" defaultValue={name} maxLength="40" onChange={handleUserName} />
+                    size="40" id="username" required minLength="2" value={name} maxLength="40" onChange={handleUserName} />
                 <span id="username-error" className="username-error form__input-error"></span>
 
                 <input name="info" className="form__field-text form__field-text_input_job" placeholder="Вид деятельности"
-                    type="text" size="40" id="status" required minLength="2"defaultValue={description}  maxLength="200" onChange={handleUserDescription} />
+                    type="text" size="40" id="status" required minLength="2" value={description}  maxLength="200" onChange={handleUserDescription} />
                 <span id="status-error" className="status-error form__input-error"></span>
             </>
         } />
